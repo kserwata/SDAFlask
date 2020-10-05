764adbc43b2fe4a054e5f2676a64e69f97c8ea8d
Kamil Serwata <k.serwata@live.com>
2020-10-05 19:35:27 +0200

    initial inherit html structure

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import json
 import os
 app = Flask(__name__)
 
+def get_menu():
+    return json.loads(open("content/menu.json").read())
+
 @app.route('/')
 @app.route('/<page>')
 def hello(page='index'):
     json_content = json.loads(open("content/pages/%s.json" % page).read())
-    return render_template('static_page.html', content=json_content)
+    return render_template('static_page.html', content=json_content, menu=get_menu())
 
 
 @app.route('/articles')
@@ -17,9 +20,9 @@ def articles():
         result = json.loads(open("content/articles/%s" % element.name).read())
         result['name'] = element.name.replace(".json", "")
         json_content.append(result)
-    return render_template('articles.html', content=json_content)
+    return render_template('articles.html', content=json_content, menu=get_menu())
 
 @app.route('/article/<name>')
 def article(name=''):
     json_content = json.loads(open("content/articles/%s.json" % name).read())
-    return render_template('article.html', content=json_content)
+    return render_template('article.html', content=json_content, menu=get_menu())
