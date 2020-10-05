4c87b8a36f507452ebe6dbb611b448356d74274d
Kamil Serwata <k.serwata@live.com>
2020-10-05 18:43:22 +0200

    create base article app

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 from flask import Flask, render_template
+import json
+import os
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('index.html', name=name)
+@app.route('/<page>')
+def hello(page='index'):
+    json_content = json.loads(open("content/pages/%s.json" % page).read())
+    return render_template('static_page.html', content=json_content)
 
 
-@app.route('/lista')
-def lista():
-    lista_uzytkownikow = [
-        { "url": "index", "title": "Strona glowna" },
-        { "url": "about", "title": "O nas"},
-        { "url": "contact", "title": "Kontakt"}
-    ]
-    return render_template('lista.html', lista=lista_uzytkownikow)
+@app.route('/articles')
+def articles():
+    json_content = []
+    for element in os.scandir("content/articles"):
+        result = json.loads(open("content/articles/%s" % element.name).read())
+        result['name'] = element.name.replace(".json", "")
+        json_content.append(result)
+    return render_template('articles.html', content=json_content)
+
+@app.route('/article/<name>')
+def article(name=''):
+    json_content = json.loads(open("content/articles/%s.json" % name).read())
+    return render_template('article.html', content=json_content)
